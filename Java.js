942c59d071e79a021a39f7a652eae8454f4c2fe0
Lilia <emrys.frogs@gmail.com>
2026-03-10 00:23:16 +0100

    Fix string comparison for 'tutu' and 'tata'

--- Java.js
+++ Java.js
@@ -52,11 +52,11 @@ function question()
     {
         document.getElementById("LA").innerHTML=nom+", Vous serez une pinte car rien ne vous ravi plus que votre teint alcoolisé !";
     }
-    else if((value1==tutu && value2=="tutu") || (value2=="tutu" && value3=="tutu") || (value1=="tutu" && value3=="tutu"))
+    else if((value1=="tutu" && value2=="tutu") || (value2=="tutu" && value3=="tutu") || (value1=="tutu" && value3=="tutu"))
     {
         document.getElementById("LA").innerHTML=nom+", Vous vous métamorphosez en verre de cantine car vous rêvez de remonter le temps !";
     }
-    else if((value1==tata && value2=="tata") || (value2=="tata" && value3=="tata") || (value1=="tata" && value3=="tata"))
+    else if((value1=="tata" && value2=="tata") || (value2=="tata" && value3=="tata") || (value1=="tata" && value3=="tata"))
     {
         document.getElementById("LA").innerHTML=nom+", Vous ne connaissez pas l'existence des verres, par conséquent, vous êtes un ver de terre !";
     }
