38253402a6b311a9ba7ff51f0d833678c826a02d
Lior <lior.beer07@gmail.com>
2026-03-09 19:28:27 +0100

    Update Java.js

--- Java.js
+++ Java.js
@@ -1,1 +1,10 @@
+function DireBonjour(x)
+{
+    return ('Bonjour ' + x+',nous allons découvrir quel type de verre vous êtes !');
+}
+nom=prompt('Quel est votre prénom svp ?');
+alert(DireBonjour(nom));
 
+function question1(){
+  
+}
