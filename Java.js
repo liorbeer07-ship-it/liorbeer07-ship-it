2a9cd33d551abea0267dfa05dc9f41ed59ba70a3
Lior <lior.beer07@gmail.com>
2026-03-09 21:17:25 +0100

    Enhance color prompt and add radio button checks

--- Java.js
+++ Java.js
@@ -7,11 +7,45 @@ alert(DireBonjour(nom));
 
 function change()
 {
-    var v=prompt("Choisissez une couleur (en anglais)");
+    var v=prompt("Choisissez une couleur (en anglais ou en hexadécimal)");
     document.body.style.backgroundColor = v;
 }
 
 function question()
 {
-  var radios1 =  document.getElementsByName('1');
+    var radios1 =  document.getElementsByName('1');
+    var radios2 =  document.getElementsByName('2');
+    var radios3 =  document.getElementsByName('3');
+    var valeur1;
+    var valeur2;
+    var valeur3;
+
+    for(var i = 0; i < radios1.length; i++)
+    {
+        if(radios1[i].checked)
+        {
+            valeur1 = radios1[i].value;
+        }
+    }
+    
+     for(var i = 0; i < radios2.length; i++)
+    {
+        if(radios2[i].checked)
+        {
+            valeur2 = radios2[i].value;
+        }
+    }
+    
+     for(var i = 0; i < radios3.length; i++)
+    {
+        if(radios3[i].checked)
+        {
+            valeur3 = radios3[i].value;
+        }
+    }
+    if((value1==toto & value2==toto) || (value2==toto & value3==toto) || (value1==toto & value3==toto))
+    {
+        
+    }
+    else if((value1==toto & value2==toto) || (value2==toto & value3==toto) || (value1==toto & value3==toto))
 }
