a145b3d1151427c2bcbffedb07d35c35be0d6ed6
Lior <lior.beer07@gmail.com>
2026-03-09 21:27:29 +0100

    Update Java.js

--- Java.js
+++ Java.js
@@ -46,10 +46,18 @@ function question()
     
     if((value1==toto & value2==toto) || (value2==toto & value3==toto) || (value1==toto & value3==toto))
     {
-        
+        document.getElementById("LA").innerHTML="Vous êtes le verre oeuf cassé, par décret du roi !";
     }
     else if((value1==titi & value2==titi) || (value2==titi & value3==titi) || (value1==titi & value3==titi))
     {
-        
+        document.getElementById("LA").innerHTML="Vous serez une pinte car rien ne vous ravi plus que votre teint alcoolisé !";
+    }
+    else if((value1==tutu & value2==tutu) || (value2==tutu & value3==tutu) || (value1==tutu & value3==tutu))
+    {
+        document.getElementById("LA").innerHTML="Vous vous métamorphosés en verre de cantine car vous rêvez de remonter le temps !";
+    }
+    else if((value1==tata & value2==tata) || (value2==tata & value3==tata) || (value1==tata & value3==tata))
+    {
+        document.getElementById("LA").innerHTML="Vous ne connaissez pas l'existence des verres, par conséquent, vou êtes un ver de terre !";
     }
 }
